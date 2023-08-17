07c389a010f050d9e9a0d5ee0ac18f1aa2922874
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-08-17 16:51:51 +0300

    Update App.jsx
    add import from contacts.json
    and add in section contacts state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,12 @@ import { Container } from './Container/Container';
 import { Section } from './Section/Section';
 import { Form } from './Form/Form';
 import { Filter } from './Filter/Filter';
+import { Contacts } from './Contacts/Contacts';
+import contacts from './data/ContactsJson.json';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: contacts.contacts,
   };
 
   formAddHandler = data => {
@@ -14,13 +16,19 @@ export class App extends Component {
   };
 
   render() {
+    console.log(this.state.contacts);
     return (
       <Container>
         <Section title={'Phonebook'}>
           <Form onSubmit={this.formAddHandler} />
         </Section>
+
         <Section title={'Filter'}>
           <Filter />
+        </Section>
+
+        <Section title={'Contacts'}>
+          <Contacts contacts={this.state.contacts}/>
         </Section>
       </Container>
     );
