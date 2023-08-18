5d0970d4b275427564c4c1d544315fca3d9312b8
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-08-18 14:32:54 +0300

    Update Container.styled.js

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -3,7 +3,6 @@ import { styled } from "styled-components";
 export const ContainerStyle = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
     background-color: #333444;
     height: 100vw;
